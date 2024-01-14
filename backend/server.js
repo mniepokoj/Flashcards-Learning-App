9fd8c19efa43c42ba3f932c43d75a360ba7c37ed
Mateusz Niepokój <mniepokoj1@gmail.com>
2024-01-14 02:15:28 +0100

    update server

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,39 @@ const db = admin.firestore();
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+
+app.get('/test', async (req, res) => 
+{
+
+  let responseCode = 200;
+  let responseMessage = 'None';
+
+  try 
+  {
+    const userRef = db.collection('user_collection').where('username', '==', 'user1');
+    const snapshot = await userRef.get();
+    if (snapshot.empty) 
+    {
+        responseMessage = {};
+    }
+    else
+    {
+    const userData = snapshot.docs[0].data();
+        responseMessage = {
+          fishcards: userData.fishcards,
+        };
+    }
+  } catch (error) 
+  {
+    console.error('Error retrieving data:', error);
+    responseCode = 500; // Ustaw kod odpowiedzi na 500 w przypadku błędu
+    responseMessage = error;
+  }
+  return res.status(responseCode).json(responseMessage);
+});
+
+app.get('/', (req, res) => 
+{
   res.send('Hello from App Enginefd!');
 });
 
