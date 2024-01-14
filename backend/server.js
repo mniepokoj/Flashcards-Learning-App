8016cff0623865e8ac525534abf290338d0ea0ad
mniepokoj <mniepokoj1@gmail.com>
2024-01-14 01:07:53 +0000

    c

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 'use strict';
+const serviceAccount = require('./serviceAccountKey.json');
+
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
 
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+  res.send('Hello from App Enginefd!');
 });
 
 const PORT = parseInt(process.env.PORT) || 8080;
